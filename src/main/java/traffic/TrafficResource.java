ede92906b4ac8bdeb77d28a063a2716bb315f012
Liu Jihao <liujihao@handge.cn>
2017-03-29 11:16:46 +0800

    add imageQuery

--- src/main/java/traffic/TrafficResource.java
+++ src/main/java/traffic/TrafficResource.java
@@ -2,10 +2,13 @@ package traffic;
 
 
 import common.InternalPools;
-import org.apache.hadoop.hbase.HRegionInfo;
-import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.client.Connection;
 
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -14,9 +17,12 @@ import org.elasticsearch.search.SearchHit;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.StringWriter;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,44 +40,27 @@ public class TrafficResource extends InternalPools {
     }
 
     @GET
-    @Path("/info")
-    @Produces("text/html;charset=utf8")
-    @Consumes("text/html;charset=utf8")
-    public Response infoQuery(@QueryParam("PlateLicens") final String PlateLicense,
-                              @QueryParam("start") final String start,
-                              @QueryParam("end") final String end) throws IOException {
-        TransportClient conn = getEsConnection();
+    @Path("/image")
+    public Response imageQuery(@QueryParam("rowkey") final String rowkey) throws IOException {
         StringWriter writer = new StringWriter();
-        HashMap content = new HashMap();
-        SearchResponse response = conn.prepareSearch().setIndices("traffic").setTypes("traffic")
-                .setQuery(QueryBuilders.termQuery("Plate_License.keyword", PlateLicense))
-                .setPostFilter(QueryBuilders.rangeQuery("Time.keyword").gte(start.replace("\"", "")).lte(end.replace("\"", "")))
-                .setSize(10000)
-                .execute().actionGet();
+        HashMap metadata = new HashMap();
 
-        for (SearchHit i : response.getHits().getHits()) {
-            HashMap metadata = new HashMap();
-            metadata.put("SBBH",i.getSource().get("SBBH").toString());
-            metadata.put("Vehicle_ChanIndex", i.getSource().get("Vehicle_ChanIndex").toString());
-            metadata.put("Plate_License", i.getSource().get("Plate_License").toString());
-            metadata.put("Vehicle_Speed", i.getSource().get("Vehicle_Speed").toString());
-            metadata.put("IllegalType", i.getSource().get("IllegalType").toString());
-            metadata.put("Dir", i.getSource().get("Dir").toString());
-            metadata.put("Vehicle_Color", i.getSource().get("Vehicle_Color").toString());
-            metadata.put("Plate_PlateType", i.getSource().get("Plate_PlateType").toString());
-            content.put(i.getSource().get("Time").toString(), metadata);
-        }
-        mapper.writeValue(writer, content);
-        returnEsConnection(conn);
-//        Connection hbase = getHbaseConnection();
-//
-//        List<HRegionInfo> regions = hbase.getAdmin().getTableRegions(TableName.valueOf("Traffic"));
-//        for (HRegionInfo info : regions) {
-//            System.out.println(info.getRegionId());
-//        }
-//        returnHbaseConnection(hbase);
-//        return Response.status(200).header("Access-Control-Allow-Origin", "*").entity(writer.toString()).build();
-        return Response.status(200).header("Access-Control-Allow-Origin", "*").header("sessionstatus",writer.toString()).entity("ok").build();
+        Connection hbase = getHbaseConnection();
+        HTable table = (HTable)hbase.getTable(TableName.valueOf("TrafficImage"));
+        Result rs = table.get(new Get(rowkey.getBytes()));
+        byte[] image = rs.getValue("Image".getBytes(), "Data".getBytes());
+        String redLightTime = new String(rs.getValue("Image".getBytes(), "RedLightTime".getBytes()));
+        String recogMode = new String(rs.getValue("Image".getBytes(), "RecogMode".getBytes()));
+        metadata.put("RedLightTime", redLightTime);
+        metadata.put("RecogMode", recogMode);
+        mapper.writeValue(writer, metadata);
+
+        returnHbaseConnection(hbase);
+        return Response.status(200)
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Content-Type","image/jpeg")
+                .header("metadata",writer.toString())
+                .entity(image).build();
     }
 
     @GET
